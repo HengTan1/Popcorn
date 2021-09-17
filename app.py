600374c5a4ce62277014e3eccfbea5a01f51d26f
credgeco <credgeco@uncg.edu>
2021-09-17 11:38:11 -0400

    Testing Login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 import popcorn_api
 
 app = Flask (__name__)
@@ -7,4 +7,12 @@ app = Flask (__name__)
 def test():
     return popcorn_api.omdb_api_call("scream")
 
+@app.route('/login')
+def login():
+    if request == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Failed Login'
+        else:
+            return redirect(url_for('login'))
+    return render_template('content.html', error=error)
 #Should just need to do "flask run" in cmd to get this to run on local host "127.0.0.1:5000"

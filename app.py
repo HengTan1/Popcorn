1279c295390ffeb6c20c8f74d9dc91c1a3412860
Chris Edgecombe <credgeco@uncg.edu>
2021-09-22 21:20:50 -0400

    Signup test html

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from enum import unique
 from flask import Flask, render_template, url_for, request
 from flask import session
 from flask import redirect
+from werkzeug.utils import html
 import popcorn_api
 #from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_manager
@@ -20,21 +21,24 @@ class User:
     def __repr__(self):
         return f'<User: {self.username}>'
 
-#Test users for now. Will need to add DB code here.
+#Test users for now. Will need to add DB code here. Check before insert.
 users = []
 users.append(User(id=1, username='Test1', password='password1'))
 users.append(User(id=2, username='Test2', password='password2'))
 users.append(User(id=3, username='Test3', password='password3'))
+users.append(User(id=4, username='Test4', password='password4'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('base.html')
 
+#Flask function that runs before each request.
 @app.before_request
 def before_request():
     g.user = None
 
     if 'user_id' in session:
+        #Need to add a DB search here for user ID.
         user = [x for x in users if x.id == session['user_id']][0]
         g.user = user
 
@@ -63,6 +67,21 @@ def profile():
 
     return render_template('profile.html')
 
+<<<<<<< Updated upstream
+=======
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    session.pop('user_id', None)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        #Would add user to DB here. create_user is a test user.
+        create_user = User(id=4, username=username, password=password)
+        session['user.id'] = create_user.id
+        return (redirect(url_for('login')))
+    return render_template('signup.html')
+
+>>>>>>> Stashed changes
 app.run()
 
 #Should just need to do "flask run" in cmd to get this to run on local host "127.0.0.1:5000"
